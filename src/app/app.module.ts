d2a8b461dbc514e97b378ae2584615cfd033352e
Ezequiel Acosta <ezekiel.acosta.94@gmail.com>
2022-10-30 00:48:33 -0300

    Se implementa routing con Angular
    - Se agrega tambien un prototipo de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,8 @@ import { HysSkillsComponent } from './components/hys-skills/hys-skills.component
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -27,7 +29,9 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
     EducacionComponent,
     HysSkillsComponent,
     ProyectosComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
