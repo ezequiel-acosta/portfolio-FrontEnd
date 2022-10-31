2bd9e3eb019317f3776ac5db90715829f769f2c9
Ezequiel Acosta <ezekiel.acosta.94@gmail.com>
2022-10-31 13:37:32 -0300

    Se implementa el BackEnd de edición del componente 'Hard & Soft Skills'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,8 @@ import { NewExperienciaComponent } from './components/experiencia/new-experienci
 import { EditExperienciaComponent } from './components/experiencia/edit-experiencia.component';
 import { NewEducacionComponent } from './components/educacion/new-educacion.component';
 import { EditEducacionComponent } from './components/educacion/edit-educacion.component';
+import { EditSkillComponent } from './components/hys-skills/edit-skill.component';
+import { NewSkillComponent } from './components/hys-skills/new-skill.component';
 
 @NgModule({
   declarations: [
@@ -41,7 +43,9 @@ import { EditEducacionComponent } from './components/educacion/edit-educacion.co
     NewExperienciaComponent,
     EditExperienciaComponent,
     NewEducacionComponent,
-    EditEducacionComponent
+    EditEducacionComponent,
+    EditSkillComponent,
+    NewSkillComponent
   ],
   imports: [
     BrowserModule,
